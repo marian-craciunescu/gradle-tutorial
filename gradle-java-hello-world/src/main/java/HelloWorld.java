500042195956585fd9da0275d8ff55a9434af936
Marian Craciunescu <m.craciunescu@tarent.de>
2019-02-05 16:36:45 +0200

    Add hello world java example

---
+++ gradle-java-hello-world/src/main/java/HelloWorld.java
@@ -0,0 +1,30 @@
+
+import org.apache.log4j.Logger;
+
+
+public class HelloWorld {
+
+ private    static Logger logger  = Logger.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+        HelloWorld obj = new HelloWorld();
+        obj.runMe("tutorial-log4j");
+    }
+
+    private void runMe(String parameter){
+
+        if(logger.isDebugEnabled()){
+            logger.debug("This is debug : " + parameter);
+        }
+
+        if(logger.isInfoEnabled()){
+            logger.info("This is info : " + parameter);
+        }
+
+        logger.warn("This is warn : " + parameter);
+        logger.error("This is error : " + parameter);
+        logger.fatal("This is fatal : " + parameter);
+
+    }
+
+}
---
+++ gradle-java-hello-world/src/test/java/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+        @Test
+        public void notARealTest() {
+            HelloWorld tester = new HelloWorld(); // MyClass is tested
+
+            // assert statements
+            assertEquals(0, 0);
+        }
+}
+
